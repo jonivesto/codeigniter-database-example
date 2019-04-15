e9d71e5a6bceedfa06b4b2574a96ba5bb78ca1ae
jonivesto <n7vejo00@students.oamk.fi>
2019-04-15 13:04:34 +0300

    MVC setup

---
+++ application/models/Example.php
@@ -0,0 +1,37 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+class Example extends CI_Model {
+	
+    // Select
+    function select($id = NULL) {
+        if(!is_null($id)){
+            $query = $this->db->get_where("example_table", array('id'=>$id));
+            return $query->row_array();
+        }else{
+            $query = $this->db->get("example_table");
+            return $query->result_array();
+        }
+    }
+    
+    // Update
+    public function update($data, $id) {
+        if(!empty($data) && !empty($id)){
+            $update = $this->db->update("example_table", $data, array('id'=>$id));
+            return $update ? true : false;
+        }else{
+            return false;
+        }
+    }
+	
+	// Delete
+    public function delete($id) {
+        $delete = $this->db->delete("example_table", array('id'=>$id));
+        return $delete ? true : false;
+    }
+	
+	// Insert
+    public function insert($data = array()) {
+        $insert = $this->db->insert("example_table", $data);
+		return $insert ? $this->db->insert_id() : false;
+    }
+    
+}
--- application/views/example.php
+++ application/views/example.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
+	<title>CodeIgniter database example</title>
 
 	<style type="text/css">
 
@@ -42,7 +42,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		color: #002166;
 		display: block;
 		margin: 14px 0 14px 0;
-		padding: 12px 10px 12px 10px;
+		padding: 3px 10px 3px 10px;
 	}
 
 	#body {
@@ -68,21 +68,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <body>
 
 <div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
-
+	<h1>CodeIgniter database example</h1>
+	
 	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
+		
+		
 
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
-
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/Welcome.php</code>
-
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
 	</div>
 
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
+	<p class="footer">Source on <a href="https://github.com/jonivesto/codeigniter-database-example">GitHub</a> | Page rendered in <strong>{elapsed_time}</strong> seconds | <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
 </div>
 
 </body>
