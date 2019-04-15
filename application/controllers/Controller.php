3541681e695dd42ddcb759014119f4fb1acf1c09
jonivesto <n7vejo00@students.oamk.fi>
2019-04-15 20:35:24 +0300

    Export database

--- application/controllers/Controller.php
+++ application/controllers/Controller.php
@@ -5,32 +5,35 @@ class Controller extends CI_Controller {
 
 	function __construct() {
         parent::__construct();
+		$this->load->helper('url');
         $this->load->model('model');
     }
 	
 	public function index(){
-
+		$data = array();
+        
+        $data['data'] = $this->model->select();
+		
         // Refresh page
         $this->load->view('blocks/header');
-        $this->load->view('view');
+        $this->load->view('view', $data);
         $this->load->view('blocks/footer');
     }
     
     // Insert
     public function insert(){
 
-		
+		redirect('controller/index', 'refresh');
     }
     
     // Update
     public function update($id){
-		
-		
+		redirect('controller/index', 'refresh');
     }
     
     // Delete
     public function delete($id){
-		
-		
+		$this->model->delete($id);
+		redirect('controller/index', 'refresh');
     }
 }
--- application/views/view.php
+++ application/views/view.php
@@ -2,11 +2,36 @@
 	<h1>CodeIgniter database example</h1>
 	
 	<div id="body">
+		<!--Table rows-->
+		<table>
+			<thead>
+				<tr>
+					<th>ID</th>
+					<th>Value</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php if(!empty($data)): foreach($data as $item): ?>
+					<tr>
+						<td><?php echo $item['id']; ?></td>
+						<td><form action="/controller/edit" method="POST"><input type="text" name="value" value="<?php echo $item['value']; ?>"><input type="submit" value="Update"></form></td>
+						<td><a href="<?php echo base_url('/controller/delete/'. $item['id']); ?>"><button>Delete</button></td>
+					</tr>
+				<?php endforeach; else: ?>
+					<tr><td colspan="4">This table is empty.</td></tr>
+				<?php endif; ?>
+			</tbody>
+		</table>
+		<br>
+		<!--Add rows-->
+		<form action="/controller/insert" method="POST">
+		  <input type="text" name="value" placeholder="New row..">
+		  <input type="submit" value="Insert">
+		</form>
 		
-		
-
 	</div>
 	
-	<p class="footer">Source on <a href="https://github.com/jonivesto/codeigniter-database-example">GitHub</a> | Page rendered in <strong>{elapsed_time}</strong> seconds | <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
+	<p class="footer">
+		Source on <a href="https://github.com/jonivesto/codeigniter-database-example">GitHub</a> | Page rendered in <strong>{elapsed_time}</strong> seconds | <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?>
+	</p>
 </div>
-
